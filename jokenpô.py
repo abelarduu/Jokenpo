ac60852395e6cae92a341b94df395d6cd8ff6fac
Abel Lucas <abelucas009@gmail.com>
2024-01-17 20:15:34 -0300

    deixando o código mais limpo

--- jokenpô.py
+++ jokenpô.py
@@ -8,7 +8,7 @@ class Card:
         self.y= y
         self.scale= scale
         self.pre_img= pygame.image.load(img)
-        self.img= pygame.transform.scale(self.pre_img, (self.pre_img.get_width()*self.scale, self.pre_img.get_height()*self.scale))
+        self.img= pygame.transform.scale(self.pre_img, (self.pre_img.get_width() * self.scale, self.pre_img.get_height()*self.scale))
         self.img_rect= self.img.get_rect(center=(self.x + self.img.get_width()/2, self.y + self.img.get_height()/2))
         self.w= self.img.get_width()
         self.h= self.img.get_height()
@@ -57,25 +57,23 @@ class Game:
     def __init__(self):
         pygame.init()
         self.screen= pygame.display.set_mode()
+        self.screen_w, self.screen_h= self.screen.get_size()
         pygame.display.set_caption("Jokenpô")
+
         self.play = True
         self.flip_card= False
-
-        self.rock_card= Card(0,0, "assets/rock_card.png",3)
-        self.paper_card= Card(0,0, "assets/paper_card.png",3)
-        self.scissors_card= Card(0,0, "assets/scissors_card.png",3)
-
-        self.rock_card2= Card(0,0, "assets/rock_card.png",3)
-        self.paper_card2= Card(0,0, "assets/paper_card.png",3)
-        self.scissors_card2= Card(0,0, "assets/scissors_card.png",3)
-
+        self.deck= Card(0,0, "assets/deck.png", 3)
         self.back_card= Card(0,0, "assets/back_card.png",3)
         self.back_card_bot= Card(0,0, "assets/back_card.png",2)
-        self.deck= Card(0,0, "assets/deck.png", 3)
-
-        self.player= Player([self.rock_card, self.paper_card, self.scissors_card])
-        self.bot= Player([self.rock_card2, self.paper_card2, self.scissors_card2])
         self.cards_on_the_table= [self.deck, self.deck]
+
+        self.player= Player([Card(0,0, "assets/rock_card.png",3),
+                             Card(0,0, "assets/paper_card.png",3),
+                             Card(0,0, "assets/scissors_card.png",3)])
+            
+        self.bot= Player([Card(0,0, "assets/rock_card.png",3),
+                             Card(0,0, "assets/paper_card.png",3),
+                             Card(0,0, "assets/scissors_card.png",3)])
 
     def verify_cards(self):
         if self.cards_on_the_table[0].type == self.cards_on_the_table[1].type:
@@ -91,10 +89,10 @@ class Game:
         if self.play:
             #Player.cards
             for card in self.player.cards:
-                self.player.update_cards(self.screen, card.w + 4 * len(self.player.cards), self.screen.get_size()[1] - card.h -10)
+                self.player.update_cards(self.screen, card.w + 4 * len(self.player.cards), self.screen_h - card.h -10)
                 self.screen.blit(card.img, (card.x, card.y))
                 
-                if card.mouse_pressed and card.y <self.screen.get_size()[1] - card.h -10:
+                if card.mouse_pressed and card.y < self.screen_h - card.h -10:
                     if not self.player.chosen_card:
                         self.cards_on_the_table[0]= self.player.select_card(card)
                    
@@ -110,16 +108,16 @@ class Game:
             for card in self.cards_on_the_table:
                 if self.flip_card: self.screen.blit(card.img, (card.x, card.y))
                 else:
-                    if card.y < self.screen.get_size()[1]-192-20:
+                    if card.y < self.screen_h-192-20:
                         self.screen.blit(self.back_card.img, (card.x, card.y))
                 card.update()
 
                 if self.player.chosen_card and self.bot.chosen_card:
-                    self.cards_on_the_table[0].x=self.screen.get_size()[0]/2 - self.screen.get_size()[0]/4 -card.w/2
-                    self.cards_on_the_table[0].y=self.screen.get_size()[1]/2 - card.h/2
+                    self.cards_on_the_table[0].x=self.screen_w/2 - self.screen_w/4 -card.w/2
+                    self.cards_on_the_table[0].y=self.screen_h/2 - card.h/2
                     
-                    self.cards_on_the_table[1].x=self.screen.get_size()[0]/2 + self.screen.get_size()[0]/4 - card.w/2
-                    self.cards_on_the_table[1].y=self.screen.get_size()[1]/2 - card.h/2
+                    self.cards_on_the_table[1].x=self.screen_w/2 + self.screen_w/4 - card.w/2
+                    self.cards_on_the_table[1].y=self.screen_h/2 - card.h/2
 
                     if pygame.mouse.get_pressed()[2]:
                         self.flip_card= True
@@ -132,8 +130,8 @@ class Game:
                         self.flip_card= False
 
             #Deck
-            self.deck.x=self.screen.get_size()[0]/2 - self.deck.w/2
-            self.deck.y=self.screen.get_size()[1]/2 - self.deck.h/2
+            self.deck.x=self.screen_w/2 - self.deck.w/2
+            self.deck.y=self.screen_h/2 - self.deck.h/2
             self.screen.blit(self.deck.img, (self.deck.x, self.deck.y))
             
     def main(self):
