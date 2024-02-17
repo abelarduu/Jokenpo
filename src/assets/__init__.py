e52e3b72d00991b5d720515ac9502267ddc1e8b9
Abel Lucas <abelucas009@gmail.com>
2024-02-17 13:47:10 -0300

    Update __init__.py

--- src/assets/__init__.py
+++ src/assets/__init__.py
@@ -5,18 +5,6 @@ from os.path import abspath, dirname
 def files_dir(folder_name:str):
     return abspath(dirname(__file__)+ f"/{folder_name}")
     
-def get_sound(name_file:str):
-    pygame.mixer.init()
-    if name_file in get_files("mp3", "sounds"):
-        sound= pygame.mixer.Sound(f"{files_dir("sounds")}/{name_file}")
-        return sound
-
-def get_music(name_file:str):
-    pygame.mixer.init()
-    if name_file in get_files("mp3", "sounds"):
-        music= pygame.mixer.music.load(f"{files_dir("sounds")}/{name_file}")
-        return music
-
 def get_files(file_extension:str, folder_name:str) ->list:
     files= listdir(files_dir(folder_name))
     for file in files:
@@ -29,3 +17,15 @@ def get_image(rect: type(pygame.Rect), scale:int):
     img_cut= TILESET.subsurface(rect)
     img_scale=pygame.transform.scale(img_cut, (img_cut.get_width()*scale, img_cut.get_height()*scale))
     return img_scale
+
+def get_sound(name_file:str):
+    pygame.mixer.init()
+    if name_file in get_files("mp3", "sounds"):
+        sound= pygame.mixer.Sound(f"{files_dir("sounds")}/{name_file}")
+        return sound
+
+def get_music(name_file:str):
+    pygame.mixer.init()
+    if name_file in get_files("mp3", "sounds"):
+        music= pygame.mixer.music.load(f"{files_dir("sounds")}/{name_file}")
+        return music
