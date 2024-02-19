0fb138f4f5f02deb345cf38d392e3e81df80ca8f
Abel Lucas <abelucas009@gmail.com>
2024-02-18 22:22:35 -0300

    verificação das cartas após vira-las na mesa

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ class Game():
     def reset(self):
         self.play= False
         self.flip_card= False
+        self.check_card= False
         self.timer= pygame.time.Clock()
         self.cards_on_the_table= [back_card, back_card2]
         self.player= Player([Card(0,0, (172,0,40,68), 3),
@@ -20,6 +21,16 @@ class Game():
                           Card(0,0, (214,0,40,68), 2),
                           Card(0,0, (257,0,40,68), 2)])
 
+    def bot_win(self):
+        self.player.get_card_from_the_deck= True
+        self.player.round_points.append(False)
+        self.bot.round_points.append(True)
+
+    def player_win(self):
+        self.bot.cards.append(self.new_card(scale=2))
+        self.player.round_points.append(True)
+        self.bot.round_points.append(False)
+
     def new_card(self, scale):
         cards_coordinates=[(172,0,40,68),(214,0,40,68),(257,0,40,68)]
         card= Card(0, 0, cards_coordinates[randint(0, len(cards_coordinates)-1)], scale)
@@ -27,30 +38,13 @@ class Game():
 
     def verify_cards(self):
         if self.cards_on_the_table[0].type == self.cards_on_the_table[1].type:
-            self.cards_on_the_table.clear()
             self.bot.cards.append(self.new_card(scale=2))
             self.player.get_card_from_the_deck= True
+        elif self.cards_on_the_table[0].type == "rock" and self.cards_on_the_table[1].type == "scissors": self.player_win()
+        elif self.cards_on_the_table[0].type == "paper" and self.cards_on_the_table[1].type == "rock": self.player_win()
+        elif self.cards_on_the_table[0].type == "scissors" and self.cards_on_the_table[1].type == "paper": self.player_win()
+        else: self.bot_win()
 
-        elif self.cards_on_the_table[0].type == "rock" and self.cards_on_the_table[1].type == "scissors":   
-            self.bot.cards.append(self.new_card(scale=2))
-            self.player.round_points.append(True)
-            self.bot.round_points.append(False)
-
-        elif self.cards_on_the_table[0].type == "paper" and self.cards_on_the_table[1].type == "rock":      
-            self.bot.cards.append(self.new_card(scale=2))
-            self.player.round_points.append(True)
-            self.bot.round_points.append(False)
-
-        elif self.cards_on_the_table[0].type == "scissors" and self.cards_on_the_table[1].type == "paper":  
-            self.bot.cards.append(self.new_card(scale=2))
-            self.player.round_points.append(True)
-            self.bot.round_points.append(False)
-        
-        else:
-            self.player.get_card_from_the_deck= True
-            self.player.round_points.append(False)
-            self.bot.round_points.append(True)
-                
     def draw_HUD(self):
         screen.blit(hud_player.img, (hud_player.pos.x, hud_player.pos.y))
         screen.blit(hud_bot.img, (hud_bot.pos.x, hud_bot.pos.y))
@@ -81,6 +75,7 @@ class Game():
                     if self.player.get_card_from_the_deck:
                         self.player.cards.append(self.new_card(scale=3))
                         self.player.get_card_from_the_deck= False
+
             #Player.cards
             for card in self.player.cards:
                 self.player.update_cards(screen_w, 4 + card.w + 4, screen_h - card.h -10)
@@ -88,7 +83,6 @@ class Game():
 
                 if card.mouse_up:
                     screen.blit(rect_pos_card.img, card.pos -(6,6))
-                    
                     if not self.player.get_card_from_the_deck:
                         if not self.player.chosen_card:
                             if card.mouse_pressed and card.pos.y < screen_h - card.h -10:
@@ -98,7 +92,7 @@ class Game():
             for card in self.bot.cards:
                 self.bot.update_cards(screen_w, 2 + back_card_bot.w + 2, 20)
                 screen.blit(back_card_bot.img, card.pos)
-            
+
                 if self.player.chosen_card and not self.bot.chosen_card:
                     card_bot= self.bot.select_card(self.bot.cards[randint(0, len(self.bot.cards)-1)])
                     self.cards_on_the_table[1]= Card(0, 0, card_bot.img_coordinates,3)
@@ -114,8 +108,12 @@ class Game():
                 if self.player.chosen_card and self.bot.chosen_card:
                     if self.flip_card:
                         screen.blit(card.img, card.pos)
-                        if pygame.mouse.get_pressed()[0]:
+
+                        if self.check_card:
                             self.verify_cards()
+                            self.check_card= False
+
+                        if pygame.mouse.get_pressed()[0]:
                             self.cards_on_the_table= [back_card, back_card2]
                             self.player.chosen_card= False
                             self.bot.chosen_card= False
@@ -125,6 +123,7 @@ class Game():
                         screen.blit(back_card.img, card.pos)
                         if card.mouse_up and pygame.mouse.get_pressed()[2]:
                             self.flip_card= True
+                            self.check_card= True
 
                 screen.blit(rect_pos_card.img, card.pos-(6,6))
                 if pygame.key.get_pressed()[K_r]:
