fab588def60576697715b97c737d5b99a9d2e7b7
Abel Lucas <abelucas009@gmail.com>
2024-08-12 13:40:40 -0300

    add sound mouse pressed

--- src/cards.py
+++ src/cards.py
@@ -1,6 +1,6 @@
 import pygame
 from pygame.math import Vector2
-from src.assets import get_image
+from src.assets import get_image, get_sound
 
 class Card():
     def __init__(self, x:int, y:int , img_coordinates:tuple, scale: int):
@@ -12,6 +12,7 @@ class Card():
         self.h = self.img.get_height()
         self.mouse_up = False
         self.mouse_pressed = False
+        self.sound_mouse_pressed = get_sound("mouse_up.mp3")
 
         #Definindo o tipo da carta
         if self.img_coordinates == (172, 0, 40, 68):
@@ -29,6 +30,7 @@ class Card():
     def update(self):
         """Verifica as interações com a carta."""
         self.img_rect = self.img.get_rect(center= (self.pos.x + self.img.get_width()/2, self.pos.y + self.img.get_height()/2))
+        
         #Mouse Up
         if self.img_rect.collidepoint(pygame.mouse.get_pos()):
             self.pos.y = (self.pos.y - 10)
@@ -37,7 +39,12 @@ class Card():
             #Mouse Pressed
             if pygame.mouse.get_pressed()[0]:
                 self.mouse_pressed = True
+                
+                if pygame.mixer.Sound.get_num_channels(self.sound_mouse_pressed) == 0:
+                    pygame.mixer.Sound.play(self.sound_mouse_pressed)
+                    
             else: 
+                pygame.mixer.Sound.stop(self.sound_mouse_pressed)
                 self.mouse_pressed = False
         
         else: 
